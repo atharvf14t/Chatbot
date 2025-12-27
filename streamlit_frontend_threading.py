7a188b9f315b7f3f8a07a33c23cc4f68bfe0bf93
atharv-asr-group <atharv21240@iiitd.ac.in>
2025-12-27 22:44:47 +0530

    multiple conversation threads

---
+++ streamlit_frontend_threading.py
@@ -0,0 +1,81 @@
+import streamlit as st
+from langgraph_backend import chatbot
+from langchain_core.messages import HumanMessage
+import uuid
+
+# ************utility functions ***********
+
+def generate_thread_id():
+    return uuid.uuid4()
+
+def reset_chat():
+    thread_id=generate_thread_id()
+    st.session_state['message_history']=[]
+    add_thread(st.session_state['thread_id'])
+    st.session_state['thread_id']=thread_id
+
+def add_thread(thread_id):
+    if thread_id not in st.session_state['chat_threads']:
+        st.session_state['chat_threads'].append(thread_id)
+
+def load_conversation(thread_id):
+    return chatbot.get_state(config={'configurable': {'thread_id': thread_id}}).values['messages']
+
+#******************* Session State Initialization ******************
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history']=[]
+
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id']=generate_thread_id()
+
+if 'chat_threads' not in st.session_state:
+    st.session_state['chat_threads']=[]
+
+add_thread(st.session_state['thread_id'])
+
+#**************************** SideBar UI ********************
+
+st.sidebar.title('LangGraph Chatbot')
+if st.sidebar.button('New Chat'):
+    reset_chat()
+st.sidebar.header('My Conversations')
+
+for thread_id in st.session_state['chat_threads']:
+    if st.sidebar.button(str(thread_id)):
+        st.session_state['thread_id']=thread_id
+        messages = load_conversation(thread_id)
+
+        temp_messages=[]
+        for msg in messages:
+            if isinstance(msg, HumanMessage):
+                role='user'
+            else: 
+                role='assistant'
+            temp_messages.append({'role':role, 'content': msg.content})
+
+        st.session_state['message_history']=temp_messages
+# ********************** Main UI ***************************
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    with st.chat_message('assistant'):
+        ai_message= st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]}, 
+                config={'configurable': {'thread_id': st.session_state['thread_id']}},
+                stream_mode='messages'
+            )
+        )
+
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+
+
